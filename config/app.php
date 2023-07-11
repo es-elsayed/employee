933d0668fc946893bccf4d263d1f4af22f18f3ed
islam-abdelkader <islamabdelkader22@gmail.com>
2023-07-11 14:20:05 +0300

    install and register spatie/laravel-permisions

--- config/app.php
+++ config/app.php
@@ -168,6 +168,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
+        // Registered Providers
+        Spatie\Permission\PermissionServiceProvider::class,
     ])->toArray(),
 
     /*
