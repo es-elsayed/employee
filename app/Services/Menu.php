cf7988d8abaa18a715430c6da28d084f4f750a53
islam-abdelkader <islamabdelkader22@gmail.com>
2023-08-07 20:34:38 +0300

    refactor nav links

---
+++ app/Services/Menu.php
@@ -0,0 +1,47 @@
+<?php
+namespace App\Services;
+
+class Menu
+{
+    static function nav()
+    {
+        return [
+            [
+                'label' => 'Dashboard',
+                'url' => route('admin.dashboard'),
+                'isActive' => request()->routeIs('admin.dashboard'),
+                'isVisible' => true,
+            ],
+            [
+                'label' => 'Permissions',
+                'url' => route('admin.permissions.index'),
+                'isActive' => request()->routeIs('admin.permissions.*'),
+                'isVisible' => request()->user()?->can('permissions-read'),
+            ],
+            [
+                'label' => 'Roles',
+                'url' => route('admin.roles.index'),
+                'isActive' => request()->routeIs('admin.roles.*'),
+                'isVisible' => request()->user()?->can('roles-read'),
+            ],
+            // [
+            //     'label' => 'Users',
+            //     'url' => route('admin.users.index'),
+            //     'isActive' => request()->routeIs('admin.users.*'),
+            //     'isVisible' => request()->user()?->can('users-read'),
+            // ],
+            // [
+            //     'label' => 'Categories',
+            //     'url' => route('admin.categories.index'),
+            //     'isActive' => request()->routeIs('admin.categories.*'),
+            //     'isVisible' => request()->user()?->can('categories-read'),
+            // ],
+            // [
+            //     'label' => 'Products',
+            //     'url' => route('admin.products.index'),
+            //     'isActive' => request()->routeIs('admin.products.*'),
+            //     'isVisible' => request()->user()?->can('products-read'),
+            // ],
+        ];
+    }
+}
--- resources/js/admin/Components/Nav/NavLinks.vue
+++ resources/js/admin/Components/Nav/NavLinks.vue
@@ -5,14 +5,8 @@ import NavLink from '@/admin/Components/Nav/NavLink.vue';
 <template>
     <!-- Navigation Links -->
     <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-        <NavLink :href="route('admin.dashboard')" :active="route().current('admin.dashboard')">
-            Dashboard
-        </NavLink>
-        <NavLink :href="route('admin.roles.index')" :active="route().current('admin.roles.*')">
-            Role
-        </NavLink>
-        <NavLink :href="route('admin.permissions.index')" :active="route().current('admin.permissions.*')">
-            Permission
+        <NavLink v-for="menu in $page.props.menus" v-show="menu.isVisible" :href="menu.url" :active="menu.isActive">
+            {{ menu.label }}
         </NavLink>
     </div>
 </template>
