24cc799c8035abddb707c1de4dd9df92c92f4767
islam-abdelkader <islamabdelkader22@gmail.com>
2023-08-09 12:37:44 +0300

    refactor input group

---
+++ resources/js/admin/Components/Form/InputGroup.vue
@@ -0,0 +1,45 @@
+<script setup>
+import Input from "./Input.vue";
+import Label from "./Label.vue";
+import Error from "./Error.vue";
+
+defineProps({
+    modelValue: {},
+    type: {
+        type: String,
+        default: "text",
+    },
+    label: {
+        type: String,
+        default: "",
+    },
+    errorMessage: {
+        type: String,
+        default: "",
+    },
+});
+</script>
+
+<template>
+    <div>
+        <Label v-if="label" :value="label" />
+        <Input :type="type" class="block w-full mt-1" :model-value="modelValue"
+            @update:model-value="$emit('update:modelValue', $event)" v-bind="$attrs" />
+        <Error v-if="errorMessage" class="mt-1" :message="errorMessage" />
+    </div>
+</template>
+<!--
+    // if you want to use every component dividual you can use the next code
+
+    <div>
+        <Label for="sample" value="Sample Text" />
+        <Input
+            id="sample"
+            v-model="form.sample"
+            type="text"
+            class="block w-full mt-1"
+            autocomplete="sample"
+        />
+        <Error :message="form.errors.sample" class="mt-2" />
+    </div>
+-->
--- resources/js/admin/Components/Form/Label.vue
+++ resources/js/admin/Components/Form/Label.vue
@@ -3,12 +3,14 @@ defineProps({
     value: {
         type: String,
     },
+    required: Boolean
 });
 </script>
 
 <template>
-    <label class="block font-medium text-sm text-gray-700">
+    <label class="block text-sm font-medium text-gray-700">
         <span v-if="value">{{ value }}</span>
+        <span v-if="required" class="text-red-600">&#160;*</span>
         <span v-else><slot /></span>
     </label>
 </template>
