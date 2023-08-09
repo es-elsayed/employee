4d264dd86a658440b3493e5d2973d40e51901c52
islam-abdelkader <islamabdelkader22@gmail.com>
2023-08-09 12:38:00 +0300

    create select input

---
+++ resources/js/admin/Components/Form/SelectGroup.vue
@@ -0,0 +1,41 @@
+<script setup>
+import Label from "./Label.vue";
+import Error from "./Error.vue";
+import Select from "./Select.vue";
+
+defineProps({
+    modelValue: {},
+    items: Array,
+    itemText: {
+        type: String,
+        default: "name",
+    },
+    itemValue: {
+        type: String,
+        default: "id",
+    },
+    withoutSelect: {
+        type: Boolean,
+        default: false,
+    },
+    label: {
+        type: String,
+        default: "",
+    },
+    errorMessage: {
+        type: String,
+        default: "",
+    },
+});
+</script>
+
+<template>
+    <div>
+        <Label v-if="label" :value="label" />
+
+        <Select class="mt-1" :model-value="modelValue" @update:model-value="$emit('update:modelValue', $event)"
+            :items="items" :item-text="itemText" :item-value="itemValue" :without-select="withoutSelect" v-bind="$attrs" />
+
+        <Error v-if="errorMessage" class="mt-1" :message="errorMessage" />
+    </div>
+</template>
